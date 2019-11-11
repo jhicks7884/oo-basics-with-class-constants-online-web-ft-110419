345ba0b1b9266989c2ec9eaf426507a5468dc0f2
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-11 22:50:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,19 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-    BRANDS = []
+  BRANDS = []
 
-  def initialize(brand)
-    @brand = brand
-  end
+ def initialize(brand)
+   @brand = brand
+   BRANDS << brand unless BRANDS.any? { |b| b == brand }
+ end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
 
-        def brands=(brands)
-          @brand = brand
-           BRANDS << brand unless BRANDS.any? { |b| b == brand }
+
+ def cobble
+   self.condition = "new"
+
  end
 end
